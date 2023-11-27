5ef5ea0bfad97bf0cdfd2f49170f9ef5bc7dbae4
Cwtorrey7 <36335140+Cwtorrey7@users.noreply.github.com>
2023-11-27 01:08:39 -0500

    Creation of gifHuntr
    First working iteration of the gifHuntr command
    
    Currently has the bot select a random GIF from a selection of 100 matching the search term.
    
    You will need to add the TENOR_KEY to your env file (or download the one I've sent)

---
+++ slash/GIFHuntr.js
@@ -0,0 +1,32 @@
+const {SlashCommandBuilder} = require("@discordjs/builders")
+const {EmbedBuilder} = require("discord.js")
+const fetch = require("node-fetch")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("gifhuntr")
+    .setDescription("Retrieves and posts a random GIF (from a selection of 10) from a given search criteria")
+    .addStringOption((option) =>
+        option
+        .setName("searchterm")
+        .setDescription("The given search criteria for the desired GIF")
+        .setRequired(true)
+    ),
+
+    run: async ({client, interaction}) => {
+        // Search term preperation
+        const search = interaction.options.getString("searchterm")
+        let term = search.trim() // trims the ending whitespace of the search term
+        const termArr = search.split(" ") // splits the search string into an array on spaces
+        term = termArr.join("-") // reconnects the words of the search term with -'s
+
+        // Defined API url with prepared term and API key 
+        let url = `https://tenor.googleapis.com/v2/search?q=${term}&key=${process.env.TENOR_KEY}&client_key=my_test_app&limit=100&contentfilter=high`
+        
+        // Recieve API response
+        let response = await fetch(url) 
+        let json = await response.json()
+        const index = Math.floor(Math.random() * json.results.length) // define a random index for which GIF to display, based on the length of results
+        await interaction.editReply(json.results[index].url) // Respond with the url from the results array at index location
+    }
+}
